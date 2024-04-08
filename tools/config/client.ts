0084e9026b670af0efc567ed833edbac02930d9b
confucian <confucian.e@gmail.com>
2024-04-08 23:15:18 +0800

    chore: switch network to sepolia

--- tools/config/client.ts
+++ tools/config/client.ts
@@ -1,12 +1,13 @@
 import dotenv from "dotenv";
 import { createWalletClient, http, type Hex } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
-import { polygonMumbai } from "viem/chains";
-import { Mumbai_RPC_HTTP } from "../../configs/networks";
+import { sepolia } from "viem/chains";
 dotenv.config();
 
 if (!process.env.PrivateKey)
   throw new Error("Please set a private key in the .env file");
+
+const Sepolia_RPC_HTTP = process.env.Sepolia_RPC_HTTP as string;
 
 const PrivateKey = process.env.PrivateKey as Hex;
 
@@ -14,6 +15,6 @@ const account = privateKeyToAccount(PrivateKey);
 
 export const walletClient = createWalletClient({
   account,
-  chain: polygonMumbai,
-  transport: http(Mumbai_RPC_HTTP),
+  chain: sepolia,
+  transport: http(Sepolia_RPC_HTTP),
 });
