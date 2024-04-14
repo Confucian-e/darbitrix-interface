ff30d362c312d248d21208c6dc01d3d6696024d0
confucian <confucian.e@gmail.com>
2024-04-14 14:27:25 +0800

    chore: add types

--- types/index.ts
+++ types/index.ts
@@ -1,28 +1,33 @@
-import type { Address } from "viem";
+import type { Address, Hex } from "viem";
 
 export type Option = {
-    value: Address,
-    label: string,
-}
+  value: Address;
+  label: string;
+};
 
 export type SwapExactForParam = {
-    amountIn: bigint,
-    amountOutMin: bigint,
-    path: Address[],
-    to: Address,
-    deadline: bigint
-}
+  amountIn: bigint;
+  amountOutMin: bigint;
+  path: Address[];
+  to: Address;
+  deadline: bigint;
+};
 
 export type SwapForExactParam = {
-    amountOut: bigint,
-    amountInMax: bigint,
-    path: Address[],
-    to: Address,
-    deadline: bigint
-}
+  amountOut: bigint;
+  amountInMax: bigint;
+  path: Address[];
+  to: Address;
+  deadline: bigint;
+};
 
 export type TokenInfo = {
-    tokenAddress: Address,
-    tokenSymbol: string,
-    tokenDecimals: number
-}
+  tokenAddress: Address;
+  tokenSymbol: string;
+  tokenDecimals: number;
+};
+
+export type Call = {
+  target: Address;
+  callData: Hex;
+};
