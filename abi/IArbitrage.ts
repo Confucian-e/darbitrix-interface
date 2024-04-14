dfba9b9c23cda1edf9b0fae730ffb0ae9cf4e765
confucian <confucian.e@gmail.com>
2024-04-14 21:28:44 +0800

    feat: add error abi

--- abi/IArbitrage.ts
+++ abi/IArbitrage.ts
@@ -2,6 +2,65 @@ export const IArbitrage = [
   {
     inputs: [
       {
+        internalType: "address",
+        name: "_vault",
+        type: "address",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "target",
+        type: "address",
+      },
+      {
+        internalType: "bytes",
+        name: "callData",
+        type: "bytes",
+      },
+      {
+        internalType: "bytes",
+        name: "ret",
+        type: "bytes",
+      },
+    ],
+    name: "CallFailed",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "caller",
+        type: "address",
+      },
+    ],
+    name: "InvalidCaller",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "length1",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "length2",
+        type: "uint256",
+      },
+    ],
+    name: "LengthNotMatch",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
         internalType: "contract IERC20[]",
         name: "tokens",
         type: "address[]",
@@ -56,6 +115,47 @@ export const IArbitrage = [
     name: "makeFlashLoan",
     outputs: [],
     stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "contract IERC20[]",
+        name: "tokens",
+        type: "address[]",
+      },
+      {
+        internalType: "uint256[]",
+        name: "amounts",
+        type: "uint256[]",
+      },
+      {
+        internalType: "uint256[]",
+        name: "feeAmounts",
+        type: "uint256[]",
+      },
+      {
+        internalType: "bytes",
+        name: "userData",
+        type: "bytes",
+      },
+    ],
+    name: "receiveFlashLoan",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "vault",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
     type: "function",
   },
   {
