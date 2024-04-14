26954b11a645a6bd147574c079c2e4ff961d4985
confucian <confucian.e@gmail.com>
2024-04-14 22:16:58 +0800

    chore: update wallet connectors

--- components/Config.tsx
+++ components/Config.tsx
@@ -1,22 +1,74 @@
 "use client";
 
 import { Arbitrum_RPC_WS } from "@/configs/networks";
-import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import {
+  connectorsForWallets,
+  RainbowKitProvider,
+} from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
+import {
+  metaMaskWallet,
+  okxWallet,
+  oneKeyWallet,
+  rabbyWallet,
+  rainbowWallet,
+} from "@rainbow-me/rainbowkit/wallets";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { WagmiProvider, webSocket } from "wagmi";
+import { createConfig, WagmiProvider, webSocket } from "wagmi";
 import { arbitrum } from "wagmi/chains";
 
-const config = getDefaultConfig({
-  appName: "My Graduation Dapp",
-  projectId: "DARBITRIX",
+/**
+ * Creates connectors for wallets.
+ *
+ * @param {Array<WalletGroup>} walletGroups - An array of wallet groups.
+ * @param {AppConfig} appConfig - The configuration for the app.
+ * @returns {Connectors} - The connectors for the wallets.
+ */
+const connectors = connectorsForWallets(
+  [
+    {
+      groupName: "Recommended",
+      wallets: [
+        metaMaskWallet,
+        rainbowWallet,
+        rabbyWallet,
+        oneKeyWallet,
+        okxWallet,
+      ],
+    },
+  ],
+  {
+    appName: "My Graduation Dapp",
+    projectId: "DARBITRIX",
+  }
+);
+
+/**
+ * Creates a configuration object for the dApp.
+ *
+ * @param {Object} options - The configuration options.
+ * @param {Array} options.connectors - The connectors to be used.
+ * @param {Array} options.chains - The chains to be used.
+ * @param {boolean} options.ssr - Whether the dApp uses server side rendering (SSR).
+ * @param {Object} options.transports - The transports to be used.
+ * @returns {Object} The configuration object.
+ */
+const config = createConfig({
+  connectors,
   chains: [arbitrum],
-  ssr: true, // If your dApp uses server side rendering (SSR)
+  ssr: true,
   transports: {
     [arbitrum.id]: webSocket(Arbitrum_RPC_WS),
   },
 });
 
+/**
+ * Config component.
+ *
+ * @param {Object} props - The component props.
+ * @param {React.ReactNode} props.children - The child components to render.
+ * @returns {JSX.Element} The rendered component.
+ */
 const Config = ({ children }: { children: React.ReactNode }) => {
   const queryClient = new QueryClient();
 
