a66f9fc018417972c517aec9a3b20d94a87c1812
confucian <confucian.e@gmail.com>
2024-05-22 15:22:19 +0800

    chore: add warning banner

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Config } from "@/components";
+import { Alert } from "antd";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Alert
+          message={
+            <span className="flex justify-center">
+              此课题仅做区块链技术研究，涉及的 Token
+              仅用作测试等用途，所设计的产品不用于实际的虚拟货币交易
+            </span>
+          }
+          type="warning"
+          banner
+          showIcon={false}
+        />
         <Config>{children}</Config>
       </body>
     </html>
